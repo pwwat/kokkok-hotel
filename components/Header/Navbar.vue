29c6f55229c6d43e15988dcf23a4206d0e489577
pwwat <pongniwat.w@gmail.com>
2020-12-24 14:32:48 +0700

    try with b-nav-toggle

--- components/Header/Navbar.vue
+++ components/Header/Navbar.vue
@@ -6,7 +6,7 @@
           <router-link :to="'/'">KOKKOK</router-link>
         </b-navbar-brand>
 
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+<!--        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>-->
 
 <!--        <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav>
